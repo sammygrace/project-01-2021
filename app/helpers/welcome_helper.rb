b2f0aa3c179f39f0fb6aa6263620e49498f9c781
Sammy Lee <sammy@Sammys-MacBook-Pro.local>
2021-01-08 13:41:07 -0800

    devise user

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,13 @@
 module WelcomeHelper
+  def hidden_when_not_signed_in
+    if !user_signed_in?
+      "display: none"
+    end
+  end
+
+  def hidden_when_signed_in
+    if user_signed_in?
+      "display: none"
+    end
+  end
 end
