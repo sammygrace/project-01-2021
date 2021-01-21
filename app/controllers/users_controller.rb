3071d5bee524a878100d7225eba4593208ae2237
Sammy Lee <sammy@Sammys-MacBook-Pro.local>
2021-01-21 12:02:26 -0800

    recent friends post on user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,8 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id]) 
     @posts = @user.posts.order(:created_at).reverse
     @friendship = current_user.friendships_as_user.find_by(friend: @user) || current_user.friendships_as_friend.find_by(user: @user)
+#    @friend_posts = Post.select { |f| @user.friends.include?(f.user) || @user.users.include?(f.user) }
+    @friends = @user.friends + @user.users
+    @friend_posts = Post.where(user: @friends).order(:created_at).reverse
   end
 end
