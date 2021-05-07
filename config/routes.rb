4439bec1f1054592c3a17a1fde65f655dc2efb3e
Sammy Lee <sammy@Sammys-MacBook-Pro.local>
2021-05-07 14:44:00 -0700

    added service name to active storage blobs fixture all tests passed!

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :conversations
+
+  resources :friendships
+
+  resources :likes, only: [:new, :show, :destroy]
+  resources :posts, only: [:index, :show, :new, :destroy]
+  resources :messages, only: [:new, :index]
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
+  resources :users, only: [:show] do
+    resources :posts
+    resources :likes
+  end
+
+  resources :friendships do
+    resources :conversations
+  end
+
+  post "/messages" => "messages#create", as: "create_message"
+
+  root to: 'welcome#index'
 end
